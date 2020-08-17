e050850fe3c9b8861d12997c29049f0441a983f3
CaiqueTorres <caiquetorres.bastos2807@gmail.com>
2020-08-17 08:06:35 -0300

    feat(1st sign up page): put the styles and the positions of that page

--- src/pages/SignUp/SignUpName/index.tsx
+++ src/pages/SignUp/SignUpName/index.tsx
@@ -1,58 +1,64 @@
 import React from 'react'
-import { Text, SafeAreaView } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { AntDesign } from '@expo/vector-icons';
-import { RectButton } from 'react-native-gesture-handler';
 
 import {
     ContainerKeyboardAvoidingView,
+    SafeAreaView,
     HeaderView,
     InfoView,
-    InputsView
+    TitleText,
+    SubtitleText,
+    InputsView,
+    NameInputText,
+    LastNameInputText,
+    InputTitleText,
+    FooterRectButton,
+    FooterRectButtonText
 } from './styles'
 
 import PageNumber from '../../../components/PageNumber'
-import LoginTextInput from '../../../components/LoginTextInput';
 
 export default function SignUpName() {
     const { navigate } = useNavigation()
 
+    function handleNavigateToLoginPage() {
+        navigate('Login')
+    }
+
     return (
         <ContainerKeyboardAvoidingView>
-            <SafeAreaView style={{
-                flex: 1,
-                backgroundColor: "#F0F0F7",
-                justifyContent: "space-between"
-            }}>
+            <SafeAreaView>
                 <HeaderView>
                     <AntDesign
                         name="arrowleft"
                         size={24}
                         color="#9C98A6"
+                        onPress={handleNavigateToLoginPage}
                     />
                     <PageNumber />
                 </HeaderView>
 
                 <InfoView>
-                    <Text>Crie a sua conta gratuíta</Text>
-                    <Text>Basta preencher esses dados e você estará conosco.</Text>
+                    <TitleText>Crie a sua conta gratuíta</TitleText>
+                    <SubtitleText>Basta preencher esses dados e você estará conosco.</SubtitleText>
                 </InfoView>
 
                 <InputsView>
-                    <Text>01. Quem é você?</Text>
-                    <LoginTextInput
+                    <InputTitleText>01. Quem é você?</InputTitleText>
+                    <NameInputText
                         placeholder="Nome"
                         transition={150}
                     />
-                    <LoginTextInput
+                    <LastNameInputText
                         placeholder="Sobrenome"
                         transition={150}
                     />
                 </InputsView>
 
-                <RectButton>
-                    <Text>Próximo</Text>
-                </RectButton>
+                <FooterRectButton>
+                    <FooterRectButtonText>Próximo</FooterRectButtonText>
+                </FooterRectButton>
             </SafeAreaView>
         </ContainerKeyboardAvoidingView>
     )
--- src/pages/SignUp/SignUpName/styles.ts
+++ src/pages/SignUp/SignUpName/styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import LoginTextInput from '../../../components/LoginTextInput';
 
 export const ContainerKeyboardAvoidingView = styled.KeyboardAvoidingView`
     flex: 1;
@@ -6,15 +9,65 @@ export const ContainerKeyboardAvoidingView = styled.KeyboardAvoidingView`
     justify-content: space-between;
 `
 
-export const HeaderView = styled.View`
-    flex-direction: row;
+export const SafeAreaView = styled.View`
+    flex: 1;
+    background: #F0F0F7;
     justify-content: space-between;
 `
 
-export const InfoView = styled.View`
+export const HeaderView = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 20px;
+`
 
+export const InfoView = styled.View`
+    width: 250px;
+`
+
+export const TitleText = styled.Text`
+    color: #32264D;
+    font-family: Poppins_600SemiBold;
+    font-weight: bold;
+    font-size: 33px;
+`
+
+export const SubtitleText = styled.Text`
+    color: #6A6180;
+    font-family: Poppins_400Regular;
+    margin-top: 15px;
 `
 
 export const InputsView = styled.View`
+    margin: 20px 0;
+`
 
+export const NameInputText = styled(LoginTextInput)`
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`
+
+export const LastNameInputText = styled(LoginTextInput)`
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+`
+
+export const InputTitleText = styled.Text`
+    color: #32264D;
+    font-family: Poppins_400Regular;
+    font-size: 30px;
+    margin-bottom: 20px;
+`
+
+export const FooterRectButton = styled(RectButton)`
+    background: #DCDCE5;
+    height: 70px;
+    border-radius: 10px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const FooterRectButtonText = styled.Text`
+    font-family: Archivo_400Regular;
+    font-size: 14px;
 `
