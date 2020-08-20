1ad084c11aa4eec7b164fbc490f76191e9043817
CaiqueTorres <caiquetorres.bastos2807@gmail.com>
2020-08-19 21:15:01 -0300

    feat(navigation): start creating the navigation of the app

--- src/pages/Presentation/FirstPage/index.tsx
+++ src/pages/Presentation/FirstPage/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function PresentationFirstPage() {
     return (
-        <View />
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Ola</Text>
+        </View>
     )
 }
