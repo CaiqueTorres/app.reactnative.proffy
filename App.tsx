db3bb699738d24119d5c4715ded062fe38f24965
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-18 14:38:24 -0300

    feat(project): removed unused files

--- App.tsx
+++ App.tsx
@@ -1,36 +1,26 @@
-import React from 'react';
-import { AppLoading } from "expo";
+import React from 'react'
 
-import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
-import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+export default function App() {}
 
-import AppStack from './src/routes/AppStack'
-import { StatusBar } from 'react-native';
+// import {
+//     Archivo_400Regular,
+//     Archivo_700Bold,
+//     useFonts
+// } from '@expo-google-fonts/archivo'
+// import {
+//     Poppins_400Regular,
+//     Poppins_600SemiBold
+// } from '@expo-google-fonts/poppins'
 
-import 'react-native-gesture-handler';
+// import { AppLoading } from 'expo'
 
-let fontsLoaded: boolean
+// import 'react-native-gesture-handler'
 
-export default function App() {
-    loadFonts()
-
-    StatusBar.setBarStyle("light-content")
-    StatusBar.setTranslucent(true)
-
-    if (!fontsLoaded) {
-        return <AppLoading />
-    }
-
-    return (
-        <AppStack />
-    );
-}
-
-function loadFonts() {
-    [fontsLoaded] = useFonts({
-        Archivo_400Regular,
-        Archivo_700Bold,
-        Poppins_400Regular,
-        Poppins_600SemiBold,
-    })
-}
+// function loadFonts() {
+//     [fontsLoaded] = useFonts({
+//         Archivo_400Regular,
+//         Archivo_700Bold,
+//         Poppins_400Regular,
+//         Poppins_600SemiBold
+//     })
+// }
