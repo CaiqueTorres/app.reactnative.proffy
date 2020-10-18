f90941812d34c76a9316f58136e261b33669ee4f
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-18 16:11:24 -0300

    feat(AppStack.tsx): add the onboarding page in app stack

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function LoginPage(): JSX.Element {
+    return (
+        //#region JSX
+
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Text>Login page</Text>
+        </View>
+
+        //#endregino
+    )
+}
