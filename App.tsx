368c427c92bf951d5326b2469c6e544f510617b6
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-18 17:51:59 -0300

    fix(App.tsx): fixed the font loading function

--- App.tsx
+++ App.tsx
@@ -4,8 +4,19 @@ import AppStack from './src/routes/AppStack'
 import { AppLoading } from 'expo'
 import { useFonts } from 'expo-font'
 
+import { Archivo_400Regular, Archivo_700Bold } from '@expo-google-fonts/archivo'
+import {
+    Poppins_400Regular,
+    Poppins_600SemiBold
+} from '@expo-google-fonts/poppins'
+
 export default function App(): JSX.Element {
-    const [fontsLoaded] = useFonts({})
+    const [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_700Bold,
+        Poppins_400Regular,
+        Poppins_600SemiBold
+    })
 
     if (!fontsLoaded) return <AppLoading />
 
