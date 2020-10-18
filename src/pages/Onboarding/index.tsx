f4a5b253a6d5da1b03e9f1dc9ec5518f8189c6a1
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-18 17:54:07 -0300

    feat(Onboarding/index.tsx & Onboarding/styles.ts): create the main style of the two first Onboarding screens

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Image, ImageBackground } from 'react-native'
+import { Image, StatusBar } from 'react-native'
 
 import Onboarding from 'react-native-onboarding-swiper'
 
-import { ContainerView, ContainerText } from './styles'
+import {
+    ContainerView,
+    IconContainerView,
+    IconContainerImageBackground
+} from './styles'
 
 import studyIcon from '../../assets/images/study.png'
 import giveClassesIcon from '../../assets/images/give-classes.png'
-import { Archivo_400Regular } from '@expo-google-fonts/archivo'
+import onboardingPageBackground01 from '../../assets/images/onboarding-page-background-01.png'
+import onboardingPageBackground02 from '../../assets/images/onboarding-page-background-02.png'
 
 /**
  * The onboarding page
  *
- * This component stores the logic and the styles and has the objective presents the app
+ * This component has the objective presents the app
  */
 export default function OnboardingPage(): JSX.Element {
     return (
@@ -20,29 +25,77 @@ export default function OnboardingPage(): JSX.Element {
 
         <ContainerView>
             <Onboarding
+                pageIndexCallback={(pageIndex: number) => {
+                    StatusBar.setBarStyle('light-content')
+                    StatusBar.setBackgroundColor(
+                        pageIndex === 0 ? '#8257E5' : '#04D361'
+                    )
+                }}
                 transitionAnimationDuration={200}
+                containerStyles={{
+                    alignItems: 'flex-start',
+                    justifyContent: 'flex-end'
+                }}
                 imageContainerStyles={{
-                    position: 'absolute',
-                    top: 150
+                    flex: 1,
+                    paddingBottom: 0
                 }}
                 titleStyles={{
                     color: '#6A618016',
-                    fontFamily: 'Archivo_400Regular'
+                    fontFamily: 'Archivo_400Regular',
+                    marginBottom: 25,
+                    marginHorizontal: 20,
+                    fontSize: 50,
+                    textAlign: 'left',
+                    marginTop: 50
                 }}
                 subTitleStyles={{
-                    color: '#6A6180'
+                    color: '#6A6180',
+                    fontFamily: 'Archivo_400Regular',
+                    marginBottom: 150,
+                    marginHorizontal: 20,
+                    fontSize: 32,
+                    textAlign: 'left'
                 }}
                 pages={[
                     {
-                        backgroundColor: '#8257e5',
-                        image: <Image source={studyIcon} />,
+                        backgroundColor: '#F0F0F7',
+                        image: (
+                            <IconContainerView backgroundImage="#8257e5">
+                                <IconContainerImageBackground
+                                    source={onboardingPageBackground01}
+                                >
+                                    <Image
+                                        source={studyIcon}
+                                        style={{
+                                            width: 175,
+                                            height: 175
+                                        }}
+                                    />
+                                </IconContainerImageBackground>
+                            </IconContainerView>
+                        ),
                         title: '01.',
                         subtitle:
                             'Encontre vários professores para ensinar você'
                     },
                     {
-                        backgroundColor: '#04D361',
-                        image: <Image source={giveClassesIcon} />,
+                        backgroundColor: '#F0F0F7',
+                        image: (
+                            <IconContainerView backgroundImage="#04D361">
+                                <IconContainerImageBackground
+                                    source={onboardingPageBackground02}
+                                >
+                                    <Image
+                                        source={giveClassesIcon}
+                                        style={{
+                                            width: 175,
+                                            height: 175
+                                        }}
+                                    />
+                                </IconContainerImageBackground>
+                            </IconContainerView>
+                        ),
                         title: '02.',
                         subtitle: 'Ou dê aulas sobre o que você mais conhece'
                     }
--- src/pages/Onboarding/styles.ts
+++ src/pages/Onboarding/styles.ts
@@ -2,7 +2,18 @@ import styled from 'styled-components/native'
 
 export const ContainerView = styled.View`
     flex: 1;
-    background: #8257e5;
 `
 
-export const ContainerText = styled.Text``
+export const IconContainerView = styled.View`
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: ${(props: { backgroundImage: string }) =>
+        props.backgroundImage};
+`
+
+export const IconContainerImageBackground = styled.ImageBackground`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`
