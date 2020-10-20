c509463a80bfe1f9ed693fbc4f91c632729a89ca
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-20 20:07:28 -0300

    feat(utils/*): add some comments in each files of utils

--- src/utils/validateText.ts
+++ src/utils/validateText.ts
@@ -1,3 +1,7 @@
+/**
+ * Function that can validate if a text is null or empty
+ * @param text stores the text string
+ */
 export default function validateText(text: string): boolean {
-    return text == '' || text == null
+    return text == '' || text == null || text.length === 0
 }
