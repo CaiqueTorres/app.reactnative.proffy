4c4a7585ca98274bbd3f9ae8a2d2578331085354
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-24 15:10:36 -0300

    feat(TextInput/index.tsx & TextInput/styles.ts): finished the main apps text input logic and style

--- src/components/atoms/TextInput/styles.ts
+++ src/components/atoms/TextInput/styles.ts
@@ -1,3 +1,46 @@
+import { Animated } from 'react-native'
+
 import styled from 'styled-components/native'
 
-export const ContainerView = styled.View``
+import AnimatedTextInput from '../AnimatedTextInput'
+
+export const ContainerView = styled.View`
+    justify-content: center;
+`
+
+export const ContainerTextInput = styled(AnimatedTextInput)`
+    background: #fff;
+    border-color: #e6e6f0;
+    border-width: 2px;
+    height: 65px;
+    padding-left: 25px;
+    color: #6a6180;
+    font-family: Poppins_400Regular;
+    font-size: 14px;
+    padding-top: 17px;
+    padding-right: ${(props: { extraPadding: number }) => props.extraPadding}px;
+`
+
+export const NonInteractableView = styled.View`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+`
+
+export const PlaceholderText = styled(Animated.Text)`
+    position: absolute;
+    left: 25px;
+    color: #9c98a6;
+    line-height: 24px;
+    font-family: Poppins_400Regular;
+`
+
+export const LineView = styled(Animated.View)`
+    position: absolute;
+    width: 100%;
+    bottom: 10px;
+    top: 10px;
+    align-self: center;
+    border-left-width: 3px;
+`
