381bea4fe7b6aec11666fb7dc623cd3883fdf38e
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-24 16:19:36 -0300

    feat(hooks/useToggle.ts): created a new hook that allow toggle some value

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+/**
+ * Hook that works as a toggle, having the opposite value each time that the
+ * user clicke on it
+ * @param initialValue stores the toggle initial value
+ */
+export default function useToggle(
+    initialValue: boolean
+): [boolean, () => void] {
+    const [active, setActive] = useState(initialValue)
+    const toggleValue = (): void => setActive(!active)
+    return [active, toggleValue]
+}
