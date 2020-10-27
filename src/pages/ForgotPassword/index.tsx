033e1ae21f967260d9dcfca8e1f64397c6118d87
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-27 14:53:42 -0300

    feat(ForgotPassword/index.tsx & ForgotPassword/styles.ts): finished the ForgotPassword page component

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,12 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+    KeyboardAvoidingView,
+    StatusBar,
+    TouchableWithoutFeedback,
+    View
+} from 'react-native'
 
-import { ContainerView } from './styles'
+import { AntDesign } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
 
+import { AppStackParamsList } from '../../routes/AppStack'
+
+import {
+    ContainerSafeAreaView,
+    IconView,
+    IconBackgroundImage,
+    LogoImage,
+    LogoSubtitleText,
+    ForgotPasswordView,
+    ForgotPasswordTitle,
+    ForgotPasswordSubtitle
+} from './styles'
+
+import Button from '../../components/atoms/Button'
+import TextInput from '../../components/atoms/TextInput'
+
+import loginPageBackgroundImage from '../../assets/images/login/login-page-background.png'
+import logoImage from '../../assets/images/logo.png'
+import { validateEmail } from '../../utils/validationFunctions'
+
+/**
+ * The app's forgot page
+ */
 const ForgotPasswordPage: React.FC = (): JSX.Element => {
+    const navigation = useNavigation<
+        StackNavigationProp<AppStackParamsList, 'ForgotPasswordPage'>
+    >()
+
+    const [valid, setValid] = useState(false)
+    const [email, setEmail] = useState('')
+
+    StatusBar.setTranslucent(true)
+    StatusBar.setBarStyle('light-content')
+
+    //#region Effects
+
+    useEffect(() => {
+        setValid(validateEmail(email))
+    }, [email])
+
+    //#endregion
+
     return (
         //#region JSX
 
-        <ContainerView />
+        <KeyboardAvoidingView behavior="position" style={{ flex: 1 }}>
+            <ContainerSafeAreaView>
+                <IconView>
+                    <IconBackgroundImage source={loginPageBackgroundImage} />
+                    <View>
+                        <LogoImage source={logoImage} />
+                        <LogoSubtitleText>
+                            Sua plaforma de estudos online.
+                        </LogoSubtitleText>
+                    </View>
+                </IconView>
+                <ForgotPasswordView>
+                    <TouchableWithoutFeedback
+                        onPress={() => {
+                            navigation.pop()
+                        }}
+                    >
+                        <AntDesign name="arrowleft" size={24} color="#9C98A6" />
+                    </TouchableWithoutFeedback>
+                    <View>
+                        <ForgotPasswordTitle>
+                            Esqueceu sua senha?
+                        </ForgotPasswordTitle>
+                        <ForgotPasswordSubtitle>
+                            Não esquenta, vamos dar um jeito nisso.
+                        </ForgotPasswordSubtitle>
+                    </View>
+                    <View>
+                        <TextInput
+                            keyboardType="email-address"
+                            placeholder="E-mail"
+                            colorTheme="#8257e5"
+                            style={{ borderRadius: 10, height: 70 }}
+                            onChangeText={setEmail}
+                        />
+                        <Button
+                            enabled={valid}
+                            enabledColor="#04D361"
+                            text="Concluir cadastro"
+                            style={{ height: 65, marginTop: 20 }}
+                            onPress={() => {
+                                navigation.push('SuccessPage', {
+                                    title: 'Redefinição enviada!',
+                                    subtitle:
+                                        'Boa, agora é só checar o e-mail que foi enviado para você redefinir sua senha e aproveitar os estudos.',
+                                    buttonTitle: 'Voltar ao login'
+                                })
+                            }}
+                        />
+                    </View>
+                </ForgotPasswordView>
+            </ContainerSafeAreaView>
+        </KeyboardAvoidingView>
 
         //#endregion
     )
