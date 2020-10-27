cd87054b2371d6c482245c58ce996e88c1ea8484
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-27 14:01:57 -0300

    feat(ForgotPassword/index.tsx & ForgotPassword/styles.ts): created the main files that will be used to create the forgot password page

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const ForgotPasswordPage: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView />
+
+        //#endregion
+    )
+}
+
+ForgotPasswordPage.displayName = 'ForgotPasswordPage'
+
+export default ForgotPasswordPage
