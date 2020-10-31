feb5206a3a155bf7c0f93246e2ae1707d4fe1237
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-31 19:27:36 -0300

    style(LandingButton/index.tsx): added some comments

--- src/pages/Landing/LandingButton/index.tsx
+++ src/pages/Landing/LandingButton/index.tsx
@@ -3,11 +3,17 @@ import { RectButtonProperties } from 'react-native-gesture-handler'
 
 import { ContainerRectButton, TitleText } from './styles'
 
+/**
+ * The app's lading page main button properties
+ */
 export interface LandingButtonProps extends RectButtonProperties {
     readonly title: string
     readonly children: JSX.Element
 }
 
+/**
+ * The app's landing page main button
+ */
 const LandingButton: React.FC<LandingButtonProps> = ({
     title,
     children,
