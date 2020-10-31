61d851455db46992bac3e1a2266f9978a4c55f8c
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-31 18:55:48 -0300

    feat(Landing/index.tsx & Landing/styles.ts): created the LandingPage style the layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,12 +1,99 @@
 import React from 'react'
+import { Image, View } from 'react-native'
 
-import { ContainerView } from './styles'
+import { AntDesign } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
+
+import { StatusBar } from 'expo-status-bar'
+
+import {
+    ContainerSafeAreaView,
+    LogoView,
+    HeaderView,
+    ProfileView,
+    ProfilePhotoImage,
+    ProfileUsernameText,
+    LogoutRectButton,
+    LandingImage,
+    ContentView,
+    TitleText,
+    SubtitleText,
+    ButtonsView,
+    FooterView,
+    FooterText
+} from './styles'
+
+import landingImage from '../../assets/images/landing.png'
+import giveClassesIcon from '../../assets/images/onboarding/give-classes.png'
+import studyIcon from '../../assets/images/onboarding/study.png'
+import profileImage from '../../assets/images/profile.jpg'
+import LandingButton from './LandingButton'
 
 const LandingPage: React.FC = (): JSX.Element => {
     return (
         //#region JSX
 
-        <ContainerView></ContainerView>
+        <ContainerSafeAreaView>
+            <StatusBar translucent style="light" />
+            <LogoView>
+                <HeaderView>
+                    <ProfileView>
+                        <ProfilePhotoImage source={profileImage} />
+                        <ProfileUsernameText>Caique Torres</ProfileUsernameText>
+                    </ProfileView>
+                    <LogoutRectButton>
+                        <Feather name="power" size={24} color="#d4c2ff" />
+                    </LogoutRectButton>
+                </HeaderView>
+                <LandingImage source={landingImage} />
+            </LogoView>
+            <ContentView>
+                <View>
+                    <TitleText>Seja bem-vindo.</TitleText>
+                    <SubtitleText>O que deseja fazer?</SubtitleText>
+                </View>
+                <ButtonsView>
+                    <LandingButton
+                        title="Estudar"
+                        style={{
+                            flex: 1,
+                            backgroundColor: '#8257E5',
+                            marginRight: 10,
+                            borderRadius: 10
+                        }}
+                    >
+                        <Image
+                            source={studyIcon}
+                            style={{
+                                width: 60,
+                                height: 60
+                            }}
+                        />
+                    </LandingButton>
+                    <LandingButton
+                        title="Dar aulas"
+                        style={{
+                            flex: 1,
+                            backgroundColor: '#04D361',
+                            marginLeft: 10,
+                            borderRadius: 10
+                        }}
+                    >
+                        <Image
+                            source={giveClassesIcon}
+                            style={{
+                                width: 60,
+                                height: 60
+                            }}
+                        />
+                    </LandingButton>
+                </ButtonsView>
+                <FooterView>
+                    <FooterText>Total de 285 conexões já realizadas</FooterText>
+                    <AntDesign name="heart" size={17} color="#B9A4EE" />
+                </FooterView>
+            </ContentView>
+        </ContainerSafeAreaView>
 
         //#endregion
     )
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,3 +1,86 @@
+import { RectButton } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 import styled from 'styled-components/native'
 
-export const ContainerView = styled.View``
+export const ContainerSafeAreaView = styled(SafeAreaView)`
+    flex: 1;
+    background: #8257e5;
+`
+
+export const LogoView = styled.View`
+    flex: 1;
+    padding: 20px 30px;
+    justify-content: space-around;
+`
+
+export const HeaderView = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const ProfileView = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const ProfilePhotoImage = styled.Image`
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+`
+
+export const ProfileUsernameText = styled.Text`
+    margin-left: 15px;
+    color: #d4c2ff;
+    font-size: 12px;
+    font-family: Poppins_400Regular;
+`
+
+export const LogoutRectButton = styled(RectButton)`
+    background: #774dd6;
+    width: 50px;
+    height: 50px;
+    border-radius: 8px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const LandingImage = styled.Image`
+    width: 100%;
+`
+
+export const ContentView = styled.View`
+    flex: 1;
+    background: #f0f0f7;
+    justify-content: space-between;
+    padding: 40px 30px;
+`
+
+export const TitleText = styled.Text`
+    font-size: 20px;
+    font-family: Poppins_400Regular;
+`
+
+export const SubtitleText = styled.Text`
+    font-size: 22px;
+    font-family: Poppins_700Bold;
+    font-weight: bold;
+`
+
+export const ButtonsView = styled.View`
+    height: 200px;
+    flex-direction: row;
+`
+
+export const FooterView = styled.View`
+    flex-direction: row;
+`
+
+export const FooterText = styled.Text`
+    color: #9c98a6;
+    font-family: Poppins_400Regular;
+    font-size: 12px;
+    margin-right: 10px;
+`
