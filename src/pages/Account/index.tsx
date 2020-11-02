828972e35248e5413bd47c9293bf912b7dd8f2d8
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-02 12:10:40 -0300

    feat(Account/index.tsx & Account/styles.ts): created the page main files

---
+++ src/pages/Account/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerSafeAreaView } from './styles'
+
+const AccountPage: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerSafeAreaView></ContainerSafeAreaView>
+
+        //#endregion
+    )
+}
+
+AccountPage.displayName = 'AccountPage'
+
+export default AccountPage
