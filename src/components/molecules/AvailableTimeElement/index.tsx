380e6e12a2468e70d21272bbc9cb66e9e3d3db17
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-07 13:05:45 -0300

    feat(AvailableTimeElement/index.tsx & AvailableTimeElement/styles.ts): created the AvailableTimeElement component main files

---
+++ src/components/molecules/AvailableTimeElement/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const AvailableTimeElement: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+AvailableTimeElement.displayName = 'AvailableTimeElement'
+
+export default AvailableTimeElement
