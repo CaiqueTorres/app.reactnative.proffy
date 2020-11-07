15f798cccc86a5134c0100350991a04e422f4b86
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-07 13:00:48 -0300

    refactor(AvailableTimesList/index.tsx & AvailableTimesList/styles.ts): moved the files to another folder

---
+++ src/components/organisms/AvailableTimesList/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import {
+    ContainerView,
+    HeaderView,
+    TitleText,
+    NewText,
+    ContentView
+} from './styles'
+
+export interface AvailableTimesListProps {
+    children?: JSX.Element
+}
+
+const AvailableTimesList: React.FC<AvailableTimesListProps> = ({
+    children
+}: AvailableTimesListProps): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView>
+            <HeaderView>
+                <TitleText>Horários disponíveis</TitleText>
+                <NewText>+ Novo</NewText>
+            </HeaderView>
+            <ContentView>{children}</ContentView>
+        </ContainerView>
+
+        //#endregion
+    )
+}
+
+AvailableTimesList.displayName = 'AvailableTimesList'
+
+export default AvailableTimesList
