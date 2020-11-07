7bdfbbcb9c705599f631b4b19714c6d4672cdf74
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-07 13:05:05 -0300

    feat(Dropdown/index.tsx & Dropdown/styles.ts): created the Dropdown main files

---
+++ src/components/atoms/Dropdown/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const Dropdown: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+Dropdown.displayName = 'Dropdown'
+
+export default Dropdown
