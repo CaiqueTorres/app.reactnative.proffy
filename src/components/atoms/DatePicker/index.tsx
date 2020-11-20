156362459071053519437617f3d7ba221c60cca2
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-20 11:14:54 -0300

    feat(DatePicker/index.tsx & DatePicker/styles.ts): created the main component files

---
+++ src/components/atoms/DatePicker/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const DatePicker: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+DatePicker.displayName = 'DatePicker'
+
+export default DatePicker
