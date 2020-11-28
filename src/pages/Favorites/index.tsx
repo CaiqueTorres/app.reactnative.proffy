5b7c13dadf0db63a38ff3dbb7c891f456b94b44a
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-11-28 10:58:41 -0300

    feat(Favorites/* & Proffys/*): created the favorite page main files; created the proffys page main files

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const FavoritesPage: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+FavoritesPage.displayName = 'FavoritesPage'
+
+export default FavoritesPage
---
+++ src/pages/Proffys/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const ProffysPage: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+ProffysPage.displayName = 'ProffysPage'
+
+export default ProffysPage
