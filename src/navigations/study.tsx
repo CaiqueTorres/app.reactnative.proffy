09dbb4d12642503ad8770c70b9862571053e5780
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-11-28 14:16:20 -0300

    feat(navigations/study.ts): styled the tab navigation tabs

--- src/navigations/study.tsx
+++ src/navigations/study.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/display-name */
 import React from 'react'
 
+import { Archivo_400Regular } from '@expo-google-fonts/archivo'
+import { Feather } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import FavoritesPage from '../pages/Favorites'
@@ -13,6 +16,12 @@ export type StudyTabNavigationParamsList = {
     readonly FavoritesPage: undefined
 }
 
+export interface TabBarIconProps {
+    focused: boolean
+    color: string
+    size: number
+}
+
 const Tab = createBottomTabNavigator<StudyTabNavigationParamsList>()
 
 /**
@@ -22,9 +31,66 @@ const StudyTabNavigation: React.FC = (): JSX.Element => {
     return (
         //#region JSX
 
-        <Tab.Navigator initialRouteName="ProffysPage">
-            <Tab.Screen name="ProffysPage" component={ProffysPage} />
-            <Tab.Screen name="FavoritesPage" component={FavoritesPage} />
+        <Tab.Navigator
+            initialRouteName="ProffysPage"
+            tabBarOptions={{
+                activeBackgroundColor: '#EBEBF5',
+                inactiveBackgroundColor: '#FAFAFC',
+                activeTintColor: '#32264C',
+                inactiveTintColor: '#C1BCCC',
+                style: { height: 74 },
+                iconStyle: {
+                    height: 50,
+                    width: 50,
+                    maxWidth: 50
+                },
+                labelStyle: {
+                    fontSize: 12,
+                    fontFamily: 'Archivo_400Regular'
+                },
+                tabStyle: {
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'row'
+                }
+            }}
+        >
+            <Tab.Screen
+                name="ProffysPage"
+                component={ProffysPage}
+                options={{
+                    tabBarLabel: 'Proffys',
+                    tabBarIcon: ({ focused, size }: TabBarIconProps) => {
+                        return (
+                            <Feather
+                                name="tv"
+                                size={size}
+                                color={focused ? '#8257E5' : '#C1BCCC'}
+                            />
+                        )
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="FavoritesPage"
+                component={FavoritesPage}
+                options={{
+                    tabBarLabel: 'Favoritos',
+                    tabBarIcon: ({ focused, size }: TabBarIconProps) => {
+                        return (
+                            <Feather
+                                name="heart"
+                                size={size}
+                                color={focused ? '#8257E5' : '#C1BCCC'}
+                            />
+                        )
+                    },
+                    tabBarBadgeStyle: {
+                        borderTopWidth: 2,
+                        borderTopColor: 'red'
+                    }
+                }}
+            />
         </Tab.Navigator>
 
         //#endregion
