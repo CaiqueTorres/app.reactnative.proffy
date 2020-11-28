23510d1d3e896f77a5f0580377a97fb2c1c75626
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-11-28 10:36:18 -0300

    feat(Study/index.tsx & Study/styles.ts): created the main study files

---
+++ src/pages/Study/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const StudyPage: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+StudyPage.displayName = 'StudyPage'
+
+export default StudyPage
