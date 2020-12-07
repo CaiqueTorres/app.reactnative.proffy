28b1ee5a5a6521da7ff1b0855f04156669bba3d2
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-07 19:59:04 -0300

    feat(Time/index.tsx & Time/styles.ts): created those basic files to work with the professor work times

---
+++ src/components/molecules/Proffy/Time/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const Time: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+Time.displayName = 'Time'
+
+export default Time
