f4244a2100862fc33908b180da51215a76dcf627
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-08 19:00:42 -0300

    feat(Proffy/index.tsx & Proffy/styles.ts): added the header component in this page and set the navigation state

--- src/pages/Proffys/index.tsx
+++ src/pages/Proffys/index.tsx
@@ -1,61 +1,28 @@
 import React from 'react'
 
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+
 import { ContainerSafeAreaView } from './styles'
 
-import Proffy from '../../components/molecules/Proffy'
+import Header from '../../components/atoms/Header'
 
-import { WeekDay } from '../../api/time'
-import profileImage from '../../assets/images/profile.jpg'
+import { AppStackParamsList } from '../../navigations/appStack'
 
 const ProffysPage: React.FC = (): JSX.Element => {
+    const navigation = useNavigation<
+        StackNavigationProp<AppStackParamsList, 'GiveClassesPage'>
+    >()
+
     return (
         //#region JSX
 
         <ContainerSafeAreaView>
-            <Proffy
-                profileImage={profileImage}
-                name="Fulano"
-                subject="Matéria"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum."
-                price={20}
-                workDays={[
-                    {
-                        id: 0,
-                        weekDay: WeekDay.MONDAY,
-                        from: '15:00',
-                        to: '17:00'
-                    },
-                    {
-                        id: 1,
-                        weekDay: WeekDay.TUESDAY,
-                        from: '15:00',
-                        to: '17:00'
-                    },
-                    {
-                        id: 2,
-                        weekDay: WeekDay.WEDNESDAY,
-                        from: '15:00',
-                        to: '17:00'
-                    },
-                    {
-                        id: 3,
-                        weekDay: WeekDay.THURSDAY,
-                        from: '15:00',
-                        to: '17:00'
-                    },
-                    {
-                        id: 4,
-                        weekDay: WeekDay.FRIDAY,
-                        from: '15:00',
-                        to: '17:00'
-                    }
-                ]}
+            <Header
+                title="Estudar"
+                onPress={() => {
+                    navigation.pop()
+                }}
             />
         </ContainerSafeAreaView>
 
@@ -66,3 +33,52 @@ const ProffysPage: React.FC = (): JSX.Element => {
 ProffysPage.displayName = 'ProffysPage'
 
 export default ProffysPage
+
+//#region Old code
+
+/* <Proffy
+    profileImage={profileImage}
+    name="Fulano"
+    subject="Matéria"
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    price={20}
+    workDays={[
+        {
+            id: 0,
+            weekDay: WeekDay.MONDAY,
+            from: '15:00',
+            to: '17:00'
+        },
+        {
+            id: 1,
+            weekDay: WeekDay.TUESDAY,
+            from: '15:00',
+            to: '17:00'
+        },
+        {
+            id: 2,
+            weekDay: WeekDay.WEDNESDAY,
+            from: '15:00',
+            to: '17:00'
+        },
+        {
+            id: 3,
+            weekDay: WeekDay.THURSDAY,
+            from: '15:00',
+            to: '17:00'
+        },
+        {
+            id: 4,
+            weekDay: WeekDay.FRIDAY,
+            from: '15:00',
+            to: '17:00'
+        }
+    ]}
+/> */
+
+//#endregion
--- src/pages/Proffys/styles.ts
+++ src/pages/Proffys/styles.ts
@@ -2,8 +2,4 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 import styled from 'styled-components/native'
 
-export const ContainerSafeAreaView = styled(SafeAreaView)`
-    justify-content: center;
-    align-items: center;
-    padding: 20px;
-`
+export const ContainerSafeAreaView = styled(SafeAreaView)``
