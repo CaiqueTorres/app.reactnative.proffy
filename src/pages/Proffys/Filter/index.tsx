0048a0ce3f0e6814d9911beeb2be17b92ccd16ba
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-10 18:19:12 -0300

    feat(Filter/index.tsx & Filter/styles.ts): created the main files of this component

---
+++ src/pages/Proffys/Filter/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+const Filter: React.FC = (): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ContainerView></ContainerView>
+
+        //#endregion
+    )
+}
+
+Filter.displayName = 'Filter'
+
+export default Filter
