c93f10df96ebd37e71245a9a9d4ec75bf7d97517
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-10 19:04:39 -0300

    feat(Filter/index.tsx): working in the filter behaviour; added the useToggle hook in filter component

--- src/pages/Proffys/Filter/index.tsx
+++ src/pages/Proffys/Filter/index.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
 import { Feather } from '@expo/vector-icons'
 import { MaterialIcons } from '@expo/vector-icons'
 
+import useToggle from '../../../hooks/useToggle'
+
 import { ContainerView, FilterText } from './styles'
 
 const Filter: React.FC = (): JSX.Element => {
+    const [active, toggle] = useToggle(false)
+
     return (
         //#region JSX
 
         <>
-            <ContainerView>
-                <Feather name="filter" size={24} color="#04D361" />
-                <FilterText>Filtrar por dia, hora e matéria</FilterText>
-                <MaterialIcons
-                    name="keyboard-arrow-down"
-                    size={24}
-                    color="#A380F6"
-                />
-            </ContainerView>
+            <TouchableWithoutFeedback onPress={toggle}>
+                <ContainerView>
+                    <Feather name="filter" size={24} color="#04D361" />
+                    <FilterText>Filtrar por dia, hora e matéria</FilterText>
+                    <MaterialIcons
+                        name={
+                            active ? 'keyboard-arrow-up' : 'keyboard-arrow-down'
+                        }
+                        size={24}
+                        color="#A380F6"
+                    />
+                </ContainerView>
+            </TouchableWithoutFeedback>
         </>
 
         //#endregion
