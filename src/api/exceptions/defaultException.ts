27d71ffdbcecf495d1a0a17163e6f45f685c210b
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-12 13:20:59 -0300

    feat(api/*): created the models and exceptions folders to organize the api payloads and proxies

---
+++ src/api/exceptions/defaultException.ts
@@ -0,0 +1,5 @@
+export interface DefaultException {
+    status: number
+    message: string
+    error: string
+}
---
+++ src/api/models/user/createUserPayload.ts
@@ -0,0 +1,6 @@
+export interface CreateUserPayload {
+    name: string
+    lastName: string
+    email: string
+    password: string
+}
