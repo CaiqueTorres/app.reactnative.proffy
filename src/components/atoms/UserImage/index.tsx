98256e10636e119b37986544728f03d233727bd9
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-13 17:41:03 -0300

    feat(UserImage/*): created the main files of this component

---
+++ src/components/atoms/UserImage/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Ionicons } from '@expo/vector-icons'
+
+import { ImageView, ProfileImage } from './styles'
+
+export interface UserImageProps {
+    readonly size: number
+    readonly imageUri?: string
+}
+
+const UserImage: React.FC<UserImageProps> = ({
+    size,
+    imageUri
+}: UserImageProps): JSX.Element => {
+    return (
+        //#region JSX
+
+        <ImageView size={size}>
+            {!imageUri ? (
+                <Ionicons name="md-person" size={100} color="#6A6180" />
+            ) : (
+                <ProfileImage source={{ uri: imageUri }} size={size} />
+            )}
+        </ImageView>
+
+        //#endregion
+    )
+}
+
+UserImage.displayName = 'UserImage'
+
+export default UserImage
