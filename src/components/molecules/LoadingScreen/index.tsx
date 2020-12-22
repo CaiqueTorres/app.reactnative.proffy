a3bdc29eb8aaa355b9bf2ad5b3be62e6b87b4f7b
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-22 14:37:17 -0300

    feat(LoadingScreen/index.tsx): removed some mocked data from the loading screen component; added some comments

--- src/components/molecules/LoadingScreen/index.tsx
+++ src/components/molecules/LoadingScreen/index.tsx
@@ -3,13 +3,28 @@ import { ActivityIndicator, Modal, ModalProps } from 'react-native'
 
 import { ContainerView } from './styles'
 
-const LoadingScreen: React.FC<ModalProps> = ({ ...rest }): JSX.Element => {
+/**
+ * The main app's loading screen properties
+ */
+export interface LoadingScreenProps extends ModalProps {
+    readonly size?: number | 'small' | 'large'
+    readonly color?: string
+}
+
+/**
+ * The main app's loading screen
+ */
+const LoadingScreen: React.FC<LoadingScreenProps> = ({
+    size = 60,
+    color = '#8257e5',
+    ...rest
+}: LoadingScreenProps): JSX.Element => {
     return (
         //#region JSX
 
         <Modal {...rest}>
             <ContainerView>
-                <ActivityIndicator size={60} color="#8257e5" />
+                <ActivityIndicator size={size} color={color} />
             </ContainerView>
         </Modal>
 
