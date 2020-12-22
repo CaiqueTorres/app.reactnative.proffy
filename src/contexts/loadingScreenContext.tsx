c6a0b0cfeb53010805630acdc676958905ab9cce
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-22 14:42:46 -0300

    feat(contexts/loadingScreenContext.tsx): added some comments in this file

--- src/contexts/loadingScreenContext.tsx
+++ src/contexts/loadingScreenContext.tsx
@@ -3,23 +3,41 @@ import { createContext } from 'react'
 
 import LoadingScreen from '../components/molecules/LoadingScreen'
 
+/**
+ * The interface that has all the needed properties that will be used to
+ * control the loading screen visibility
+ */
 export interface LoadingScreenData {
     enabledLoading: boolean
     setEnabledLoading: (value: boolean) => void
 }
 
-export interface LoadingScreenProviderProps {
-    children: JSX.Element
-}
-
+/**
+ * The context that allows all the app components change the loading screen
+ * visibility
+ */
 export const LoadingScreenContext = createContext<LoadingScreenData>(
     {} as LoadingScreenData
 )
 
+/**
+ * The loading screen provider properties
+ */
+export interface LoadingScreenProviderProps {
+    children: JSX.Element
+}
+
+/**
+ * The provider that can control the loading screen visibility
+ */
 export const LoadingScreenProvider: React.FC<LoadingScreenProviderProps> = ({
     children
 }: LoadingScreenProviderProps): JSX.Element => {
+    //#region States
+
     const [enabledLoading, setEnabledLoading] = useState(false)
+
+    //#endregion
 
     return (
         //#region JSX
