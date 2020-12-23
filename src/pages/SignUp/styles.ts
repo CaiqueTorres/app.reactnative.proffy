bc7070001e01f12a8413005f951ca39757f349fb
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-12-22 21:33:20 -0300

    feat(SignUp/styles.ts): added the heightPercentageToDP function to allow make this page responsive

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,5 @@
 import { Dimensions } from 'react-native'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import styled from 'styled-components/native'
@@ -7,25 +8,27 @@ export const ContainerSafeAreaView = styled(SafeAreaView)`
     height: ${Dimensions.get('screen').height}px;
     justify-content: space-between;
     align-items: center;
-    padding: 50px 30px;
+    padding: 60px 30px;
 `
 
 export const TitleView = styled.View`
-    margin-top: 150px;
+    margin-top: 10%;
     width: 100%;
 `
 
 export const TitleText = styled.Text`
     color: #32264d;
     font-family: Poppins_600SemiBold;
-    font-size: 32px;
+    font-size: ${hp('4.5%')}px;
+    width: 85%;
     margin-bottom: 13px;
 `
 
 export const SubtitleText = styled.Text`
     color: #6a6180;
     font-family: Poppins_400Regular;
-    font-size: 14px;
+    font-size: ${hp('2%')}px;
+    width: 80%;
 `
 
 export const FooterView = styled.View`
@@ -36,7 +39,7 @@ export const FooterView = styled.View`
 export const FooterTitle = styled.Text`
     color: #32264d;
     font-family: Poppins_600SemiBold;
-    font-size: 24px;
+    font-size: ${hp('3%')}px;
 `
 
 export const TextsInputView = styled.View`
