15c5ca212f69613ee0f7a9066b13a5651148ddc0
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-30 16:16:49 -0300

    feat(authService.ts & subjectService.ts & timeService.ts & userService.ts): moved all the api connection logic to class methods, instead of only functions

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,11 +3,13 @@ import { TokenProxy } from '../models/auth/tokenProxy'
 
 import api from '../api'
 
-/**
- * Function that can return the token
- * @param payload stores the user credentials
- */
-export async function login(payload: LoginPayload): Promise<TokenProxy> {
-    const response = await api.post<TokenProxy>('/auth/local', payload)
-    return response.data
+export class AuthService {
+    /**
+     * Function that can return the token
+     * @param payload stores the user credentials
+     */
+    public async login(payload: LoginPayload): Promise<TokenProxy> {
+        const response = await api.post<TokenProxy>('/auth/local', payload)
+        return response.data
+    }
 }
--- src/services/subjectService.ts
+++ src/services/subjectService.ts
@@ -3,41 +3,41 @@ import { SubjectProxy } from '../models/subject/subjectProxy'
 
 import api from '../api'
 
-/**
- * Function that can get the subjects in the database with pagination
- * @param offset stores the initial value position
- * @param limit stores the page size
- * @param page stores the current page
- * @param token stores the logged user token
- */
-export async function getSubjectsAsGetManyDefaultResponse(
-    offset: number,
-    limit: number,
-    page: number,
-    token: string
-): Promise<GetManyDefaultResponse<SubjectProxy>> {
-    const response = await api.get<GetManyDefaultResponse<SubjectProxy>>(
-        `/subjects?offset=${offset}&limit=${limit}&page=${page}`,
-        {
+export class SubjectService {
+    /**
+     * Function that can get the subjects in the database with pagination
+     * @param offset stores the initial value position
+     * @param limit stores the page size
+     * @param page stores the current page
+     * @param token stores the logged user token
+     */
+    public async getSubjectsAsGetManyDefaultResponse(
+        offset: number,
+        limit: number,
+        page: number,
+        token: string
+    ): Promise<GetManyDefaultResponse<SubjectProxy>> {
+        const response = await api.get<GetManyDefaultResponse<SubjectProxy>>(
+            `/subjects?offset=${offset}&limit=${limit}&page=${page}`,
+            {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            }
+        )
+        return response.data
+    }
+
+    /**
+     * Function that can get all the subjects saved in the database
+     * @param token stores the logged user token
+     */
+    public async getAllSubjectsAsArray(token: string): Promise<SubjectProxy[]> {
+        const response = await api.get<SubjectProxy[]>(`/subjects`, {
             headers: {
                 Authorization: 'Bearer ' + token
             }
-        }
-    )
-    return response.data
-}
-
-/**
- * Function that can get all the subjects saved in the database
- * @param token stores the logged user token
- */
-export async function getAllSubjectsAsArray(
-    token: string
-): Promise<SubjectProxy[]> {
-    const response = await api.get<SubjectProxy[]>(`/subjects`, {
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    })
-    return response.data
+        })
+        return response.data
+    }
 }
