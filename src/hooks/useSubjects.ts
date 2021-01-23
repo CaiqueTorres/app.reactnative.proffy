597bcab5a4891e776ff67ba169d7d75eb4aedb79
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-01-23 15:19:20 -0300

    feat(store/subjects/* & useSubjects.ts): removed those unused files

--- src/hooks/useSubjects.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux'
-
-import { SubjectProxy } from '../models/subject/subjectProxy'
-
-import { RootState } from '../store/rootReducer'
-
-export default function useSubjects(): SubjectProxy[] | null {
-    return useSelector((state: RootState) => state.subjectState.subjects)
-}
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,4 +1,3 @@
-import { subjectReducer } from './subjects/reducer'
 import { userReducer } from './user/reducers'
 import { combineReducers } from 'redux'
 
@@ -6,8 +5,7 @@ import { combineReducers } from 'redux'
  * The app's main reducer
  */
 export const rootReducer = combineReducers({
-    userState: userReducer,
-    subjectState: subjectReducer
+    userState: userReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
